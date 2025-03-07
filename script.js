c50196667dcd0cfb8827193640d34a91baa8bdb6
Vinicius <vinispmm@hotmail.com>
2025-03-07 00:55:45 -0300

    popup feito

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+function mostrarPopup(titulo, receita) {
+    document.getElementById('popup-titulo').innerText = titulo;
+    document.getElementById('popup-texto').innerText = receita;
+    document.getElementById('popup').style.display = 'flex';
+}
+
+document.getElementById('popup').addEventListener('click', function(event) {
+    if (event.target === this) { 
+        esconderPopup();
+    }
+});
+
+function esconderPopup() {
+    document.getElementById('popup').style.display = 'none';
+}
+
 let list = document.querySelectorAll('.item');
 let next = document.getElementById('prox');
 let prev = document.getElementById('ant');
